89e4f018ab5176328e831478df541e566545ef62
Anusree <anusree@weetechsolution.com>
2025-03-26 15:54:34 +0530

    feat: added docker learning demo

--- docker/practise/nodejs-docker-image/src/index.js
+++ docker/practise/nodejs-docker-image/src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 app.use("/", (req, res) => {
-  console.log("route called");
-  res.send('docker route called')
+  console.log("route called in replica:", process.env.APP_NAME);
+  res.send("docker route called");
 });
+
 app.listen(3000, () => {
-  console.log("server is running on port 3000");
+  console.log(process.env.APP_NAME, " is running on port 3000");
 });
